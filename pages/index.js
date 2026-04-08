cd2e5d94ec938ad4a80ef5766db4b4e303474faf
Yahonan Alcântara <110312787+Yahonan@users.noreply.github.com>
2026-04-08 01:32:09 +0000

    adiciona a página inicial do projeto

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+function Home(){
+    return( 
+    <div>
+        <h1>Perserverar, se você ganhar todos ganham junto, inclusive o desejo de morar com o seu amor</h1>
+        <h1>Oi mo tinhamo tim amo tim amor bem muitao muitao</h1>
+    </div>
+    )
+};
+
+export default Home;
